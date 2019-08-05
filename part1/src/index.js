6f6a2aa2ef1c7a9fca808a14192e5c6a3e48f12e
musavy <sayed.musavy@integrify.io>
2019-08-05 13:57:13 +0300

    first commit

---
+++ part1/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+
+const App=()=>{
+    const course='Half Stack application development'
+    const part1='Fundamentals of React'
+    const exercises1=10
+    const part2='Using props to pass data'
+    const exercisese2=7
+    const part3='State of a component'
+    const exercises3=14
+
+    return ()
+    
+        }
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
